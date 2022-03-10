1c2f175524f9a1615b1d586066992a0a3483b5be
Carmen <carmenp.gomez99@gmail.com>
2022-03-10 17:03:07 +0100

    feat(burger-menu.js copied through gulp to dist)

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,8 @@
 module.exports = config => {
     config.addPassthroughCopy('./src/images/');
-    // Returns work items, sorted by display order
-    const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
-    // Returns work items, sorted by display order
-    config.addCollection('work', collection => {
-      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
-    });
 
-    // Returns work items, sorted by display order then filtered by featured
-    config.addCollection('featuredWork', collection => {
-      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
-        x => x.data.featured
-      );
-    });
+    config.addPassthroughCopy('./src/assets/scripts/');
 
-    // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
     config.setUseGitIgnore(false);
 
     return {
